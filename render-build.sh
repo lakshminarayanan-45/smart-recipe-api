f6c2257916a72a7cfbd87972358aea6036f5c482
lakshminarayanan-45 <lakshminarayanan45074507@gmail.com>
2025-07-23 15:50:02 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
+
+# Build Vite frontend
+cd Frontend
+npm install
+npm run build
+
+# Move built files
+cd ..
+mkdir -p templates static
+cp dist/index.html templates/
+cp -r dist/assets static/
