5e5a9ba95663ef318d783fd46d8e481b36a1d686
lakshminarayanan-45 <lakshminarayanan45074507@gmail.com>
2025-07-23 15:58:31 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -34,3 +34,7 @@ def scale_recipe():
         return jsonify(result)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render assigns dynamic port
+    app.run(host="0.0.0.0", port=port)
